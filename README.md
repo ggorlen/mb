dfbd5be788c8aad0d3bf9defc797462f8ab0bf3b
ggorlen <gsgorlen@gmail.com>
2017-12-29 16:51:53 -0800

    site redesign alpha

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # mb
 Mission Bit class material, games and scripts
+
+## todo
++ Back to top button in modules: https://stackoverflow.com/questions/5980237/show-back-to-top-link-element-using-jquery-when-you-scroll-down
++ Add Phaser section in course material
++ Navbar appear http://jsfiddle.net/panchroma/nwV2r/
--- js/global.js
+++ js/global.js
@@ -1,16 +1,42 @@
+"use strict";
+
+
 $(document).ready(function () {
 
+  // Make the page visible
+  document.body.style.opacity = 1;
+
+  // Style nav buttons
+  const navBtns = document.getElementsByTagName("nav")[0].children;
+
+  for (let i = 0; i < navBtns.length; i++) {
+    if (navBtns[i].tagName === "A") {
+      navBtns[i].style.background = "hsl(" + (360 - i * 80) + ", 80%, 50%)";
+    }
+  }
+
+  // Sticky nav
+  const nav = $("nav");
+  const navInitOffset = nav.offset().top;
+  const stickyNav = function () {
+    if ($(window).scrollTop() >= navInitOffset) { nav.addClass("sticky"); } 
+    else { nav.removeClass("sticky"); }
+  };
+  
+  stickyNav();
+  $(window).scroll(stickyNav);
+
   // Highlight code with HLJS
-  $('code').each(function (i, block) {
-    hljs.highlightBlock(block);
+  $("code").each(function (i, block) { 
+    hljs.highlightBlock(block); 
   });
 
   // Animated scrolling effects
-  $(document).on("click", "a", function (event) {
+  $(document).on("click", "a", function (e) {
     let destination = $.attr(this, "href");
   
     if (destination.startsWith("#")) {
-      event.preventDefault();
+      e.preventDefault();
   
       $("html, body").animate({
         scrollTop: $(destination).offset().top
