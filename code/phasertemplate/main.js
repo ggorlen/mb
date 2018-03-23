5399fd1572ff5bc455fbdf9dd74218f611cb0400
ggorlen <gsgorlen@gmail.com>
2018-03-23 09:26:09 -0700

    refactor game state example

--- code/phasertemplate/main.js
+++
@@ -1,9 +0,0 @@
-// Create the Phaser game object
-const game = new Phaser.Game(800, 600, Phaser.AUTO, "example");
-
-// Add game states
-game.state.add("menu", menuState);
-game.state.add("game", gameState);
-
-// Start the menu state
-game.state.start("menu");
---
+++ code/phasertemplates/states/menu.js
@@ -0,0 +1,40 @@
+/**
+ * menu.js 
+ *
+ * This is the menu state
+ */
+const menuState = {
+
+  /**
+   * Loads game assets (images, sounds, tilemaps, etc)
+   */
+  preload : function () {
+
+    // Enable mouse
+    game.input.mouse.capture = true;
+  },
+
+  /**
+   * Initializes variables and instantiates objects
+   */
+  create: function () {
+
+    // Add some text
+    game.add.text(
+      80, 260,  // x, y position
+      "Welcome to the menu state. Click to begin!", 
+      { fontSize: "32px", fill: "#fff" }
+    );
+  },
+
+  /** 
+   * Updates the screen each frame
+   */
+  update: function () {
+
+    // Check for mouse click and switch state to the game state when
+    if (game.input.activePointer.leftButton.isDown) {
+      game.state.start("game");
+    }
+  }
+}; // end menuState
