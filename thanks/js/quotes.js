d8308ee3a744c918e8444b123b08612bfc9a3d55
ggorlen <gsgorlen@gmail.com>
2018-05-08 17:12:47 -0700

    remove last line in quotes.txt

--- thanks/js/quotes.js
+++ thanks/js/quotes.js
@@ -6,6 +6,7 @@ function onSuccess(responseText) {
 
   // Make a div for each quote
   responseText.split("\n").forEach(function (e) {
+  console.log(e);
     var quoteElem = document.createElement("div");
     var quote = e.split("#");
     quoteElem.innerHTML = 
